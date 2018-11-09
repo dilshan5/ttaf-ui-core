b273876f5b56f2d7a643591b03a534168cbfdfa8
dilshan <dilshan.fdo@gmail.com>
2018-11-09 17:11:21 +0530

    update date

--- src/main/java/com/automation/qa/ttafuicore/page/BasicPage.java
+++ src/main/java/com/automation/qa/ttafuicore/page/BasicPage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 
 /**
  * Introduces an additional abstract layer for real Page classes template, every page class should be extended from this
- * Created by DilshanF on 10/23/2018.
+ * Created by DilshanF on 11/06/2018.
  */
 public class BasicPage extends TestBase {
     protected WebDriver driver;
