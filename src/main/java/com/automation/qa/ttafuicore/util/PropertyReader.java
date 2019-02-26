f45795b4c7a0fbb1a3b296243d7a1a74ba689f94
Dilshan Fernando <DilshanF@Zone24x7.lk>
2019-02-26 15:45:33 +0530

    Add PropertyReader clas

---
+++ src/main/java/com/automation/qa/ttafuicore/util/PropertyReader.java
@@ -0,0 +1,43 @@
+package com.automation.qa.ttafuicore.util;
+
+import org.apache.log4j.Logger;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Properties;
+
+public class PropertyReader {
+    private static final Logger LOGGER = Logger.getLogger(String.valueOf(PropertyReader.class));
+    private static BufferedReader reader;
+    private static Properties properties;
+    private static URL congfigFile = null;
+
+    static {
+        properties = new Properties();
+        // the configuration file name
+        String fileName = "config/Configuation.properties";
+        congfigFile = Constant.class.getClassLoader().getResource(fileName);
+
+
+        try {
+            reader = new BufferedReader(new FileReader(congfigFile.getPath()));
+            properties = new Properties();
+            try {
+                properties.load(reader);
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            LOGGER.error("Configuration.properties not found at " + fileName, e);
+        }
+    }
+    public static  <E> E getValue(String key) {
+        E keyValue;
+        return keyValue = (E) properties.getProperty(key);
+    }
+
+}
