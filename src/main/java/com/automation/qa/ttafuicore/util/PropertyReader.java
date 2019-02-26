43a79113dcf601e3561216db483404c4680eb063
Dilshan Fernando <DilshanF@Zone24x7.lk>
2019-02-26 19:44:11 +0530

    sample commit

--- src/main/java/com/automation/qa/ttafuicore/util/PropertyReader.java
+++ src/main/java/com/automation/qa/ttafuicore/util/PropertyReader.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.Properties;
 
+//commit to do sample 
 public class PropertyReader {
     private static final Logger LOGGER = Logger.getLogger(String.valueOf(PropertyReader.class));
     private static BufferedReader reader;
