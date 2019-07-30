75f9c4af56800a72a0f4387f46125ab838dc78f0
Dilshan Fernando <dilshan.fernando@syscolabs.com>
2019-07-30 15:51:42 +0530

    Add Allure.parameter

--- src/main/java/com/automation/qa/ttafuicore/driver/DriverManager.java
+++ src/main/java/com/automation/qa/ttafuicore/driver/DriverManager.java
@@ -1,6 +1,7 @@
 package com.automation.qa.ttafuicore.driver;
 
 import com.automation.qa.ttafuicore.util.Constant;
+import io.qameta.allure.Allure;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -21,6 +22,7 @@ For more details please refer to the src link above :)
         if (driver.get() == null) {
             // this is need when running tests from IDE
             LOGGER.info("Thread has no WedDriver, creating new one");
+            Allure.parameter("Browser", Constant.BROWSER_NAME);
             DriverFactory.createInstance(Constant.BROWSER_NAME, Constant.BROWSER_VERSION, Constant.PLATFORM);
         }
         LOGGER.info("Getting instance of remote driver" + driver.get().getClass());
