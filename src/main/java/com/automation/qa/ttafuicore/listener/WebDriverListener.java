a11786d89b0b0ec461431c13b01b647aa50a8be2
Dilshan Fernando <dilshan.fdo@gmail.com>
2019-08-26 08:52:32 +0530

    rem

--- src/main/java/com/automation/qa/ttafuicore/listener/WebDriverListener.java
+++ src/main/java/com/automation/qa/ttafuicore/listener/WebDriverListener.java
@@ -1,6 +1,5 @@
 package com.automation.qa.ttafuicore.listener;
 
-import com.automation.qa.ttafuicore.driver.DriverManager;
 import com.automation.qa.ttafuicore.driver.DriverFactory;
 import com.automation.qa.ttafuicore.util.Constant;
 import org.testng.IInvokedMethod;
